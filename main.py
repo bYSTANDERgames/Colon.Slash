95219009be39b7b17200ed5ceba4c9a55eb25be1
Zain-Itoko <71819797+bYSTANDERgames@users.noreply.github.com>
2023-02-08 13:27:23 -0800

    Added Variable Adding (Define)
    Makes File
    Defines Variable

--- main.py
+++ main.py
@@ -123,9 +123,18 @@ elif line2 == "::/file/delete":
 
 #line2 adding variable and then adding to file
 elif line2 == "::/create/variable/filename/line3/variablename/line4":
+    print("Made File")
+    f = open(line3, "w")
+    f.write(line4)
+    f.close()
     with open(line3, 'w') as f:
         f.write(line4)
+
 elif line2 == "::/create/variable/filename/line4/variablename/line3":
+    print("Made File")
+    f = open(line4, "w")
+    f.write(line3)
+    f.close()
     with open(line4, 'w') as f:
         f.write(line3)
 
