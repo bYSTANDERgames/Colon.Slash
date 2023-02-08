db741ffa3d55555d78471389417183a374a02782
Zain-Itoko <71819797+bYSTANDERgames@users.noreply.github.com>
2023-02-08 13:42:57 -0800

    Fixed Bug

--- main.py
+++ main.py
@@ -137,12 +137,14 @@ elif line2 == "::/create/variable/filename/line4/variablename/line3":
 #line2 adding variable value
 elif line2 == "::/create/variablevalue/filename/line3/variablevalue/line4":
     with open(line3, 'a') as f:
-        f.write('\n'.join(line4))
+        line4_w = ["", line4]
+        f.write('\n'.join(line4_w))
     print("Edited")
 
 elif line2 == "::/create/variablevalue/filename/line4/variablevalue/line3":
     with open(line4, 'a') as f:
-        f.write('\n'.join(line3))
+        line3_w = ["", line3]
+        f.write('\n'.join(line3_w))
     print("Edited")
 
 #line2 adding variable error checking
